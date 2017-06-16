4c97febad1fbc4dae3a275533bdf0a5e952199f5
seth loh <haloandwings@gmail.com>
2017-06-16 09:35:23 +0800

    hmmm

--- app/components/docs/pages/intro/Causes.jsx
+++ app/components/docs/pages/intro/Causes.jsx
@@ -9,7 +9,7 @@ const Causes = () => (
       Beginning with Repair Kopitiam, an island-wide initiative to bring people together to repair their home appliances, clothes, furniture, and toys, I found great joy in purposing my handyman skills to breathe new life into damaged items. You can read about my story <a href="http://repairkopitiam.sg/repair-stories/seth-loh/"><strong><u>here</u></strong></a>
     </p>
     <p>
-      I founded the Citizen Makers, an informal citizen arts collective questioning technocentric narratives, support community arts groups, Yarnbombing Singapore, with pro-bono web design and funding advisory.
+      I founded the Citizen Makers, an informal citizen arts collective questioning technocentric narratives, and help to support community arts groups, such as Yarnbombing Singapore, with pro-bono web design and funding advisory.
     </p>
     <p>
       If you have a great cause to share, you can always reach me <a href="mailto:lackdaz@gmail.com?Subject=Hello%20Seth"><strong><u>here</u></strong></a>
